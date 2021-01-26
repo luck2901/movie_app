7afff5a8485ab23d2a8dce2eb824d1610a696f7e
unknown <luck2901@naver.com>
2021-01-26 21:57:02 +0900

    Final

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -7,6 +7,7 @@ function Navigation(){
         <div><Link to="/about">About</Link> </div>
         {/* Link는 라우터안에서만 사용 가능. */}
         {/* <a href="/">Home</a>
+        주의
         <a href="/about">About</a>  페이지가 새로고침되면서 넘어가기에 Link사용*/}
     </div>
 }
